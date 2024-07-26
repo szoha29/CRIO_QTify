01c76e68647dfe598d6e26c71c5e85230370f1d7
szoha2095 <szoha2095@gmail.com>
2024-07-27 00:55:21 +0530

    Updated code change

--- qtify/src/Search/Search.jsx
+++ qtify/src/Search/Search.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./Search.module.css";
 import { ReactComponent as SearchIcon } from "../assets/search-icon.svg";
-import useAutocomplete from "@mui/base/useAutocomplete";
+import { useAutocomplete } from "@mui/base/useAutocomplete";
 import { styled } from "@mui/system";
 // import { truncate } from "../../helpers/helpers";
 import _ from 'lodash';
