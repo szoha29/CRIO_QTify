7c9c2158f3abf750f9ef093aab5843aef5925415
szoha2095 <szoha2095@gmail.com>
2024-07-26 22:21:42 +0530

    Hero Image path corrected in Hero.js

--- qtify/src/Hero/Hero.jsx
+++ qtify/src/Hero/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
       </div>
       <div>
         <img
-          src={require("../../assets/hero_headphones.png")}
+          src={require("../assets/hero_headphones.png")}
           width={212}
           alt="headphones"
         />
