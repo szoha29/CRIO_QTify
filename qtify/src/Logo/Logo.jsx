06b8324c075b7920a684b7f329fc3753e3e3e164
szoha2095 <szoha2095@gmail.com>
2024-07-26 23:27:36 +0530

    Update logo path in Logo.js

--- qtify/src/Logo/Logo.jsx
+++ qtify/src/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LogoImage from "../../assets/logo.png";
+import LogoImage from "../assets/logo.png";
 
 export default function Logo() {
   return <img src={LogoImage} alt="logo" width={67} />;
