f839ae7115aa3cc423a0048f8038182d88b6d105
szoha2095 <szoha2095@gmail.com>
2024-07-29 08:31:54 +0530

    Removed SongsSection component from App.js

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Navbar from './Navbar/Navbar'; 
 import Hero from './Hero/Hero'; 
 import Section from './Section/Section';
-import SongsSection from './SongsSection/SongsSection';
 
 function App() {
   return (
@@ -12,7 +11,7 @@ function App() {
       <Hero />
       <Section title="Top Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/top" />
       <Section title="New Albums" apiEndpoint="https://qtify-backend-labs.crio.do/albums/new" />
-      <SongsSection />
+      <Section title="Songs" apiEndpoint="https://qtify-backend-labs.crio.do/songs" />
     </div>
   );
 }
