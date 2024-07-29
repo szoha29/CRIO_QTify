6d79d5a51dbd47471af2db5fba64d61921992097
szoha2095 <szoha2095@gmail.com>
2024-07-29 10:46:33 +0530

    Reverted to stable code of Carousel.js

--- qtify/src/Carousel/Carousel.jsx
+++ qtify/src/Carousel/Carousel.jsx
@@ -1,22 +1,18 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import { Navigation } from 'swiper'; // Corrected import for Navigation
+import { Navigation } from "swiper/modules";
+import "swiper/swiper-bundle.css";
+import "./Carousel.css";
 import LeftNavButton from "../LeftNavButton/LeftNavButton";
 import RightNavButton from "../RightNavButton/RightNavButton";
-import "../Carousel/Carousel.css";
 
 const Carousel = ({ items }) => {
-  const swiperRef = useRef(null);
-
   return (
     <div className="carousel-container">
       <Swiper
         modules={[Navigation]}
         spaceBetween={20}
         slidesPerView={"auto"}
-        onSwiper={(swiper) => (swiperRef.current = swiper)}
         navigation={{
           nextEl: ".right-nav-button",
           prevEl: ".left-nav-button",
@@ -37,8 +33,8 @@ const Carousel = ({ items }) => {
           <SwiperSlide key={index}>{item}</SwiperSlide>
         ))}
       </Swiper>
-      <LeftNavButton swiperRef={swiperRef} />
-      <RightNavButton swiperRef={swiperRef} />
+      <LeftNavButton />
+      <RightNavButton />
     </div>
   );
 };
